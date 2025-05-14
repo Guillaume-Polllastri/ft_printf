567951e0fa77d05cf4be1ba43e3a3d04066c8013
Guillaume Pollastri <gpollast@f6r6s5.clusters.42paris.fr>
2025-05-14 11:05:30 +0200

    ajout include var args

--- libftprintf.h
+++ libftprintf.h
@@ -6,12 +6,14 @@
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 16:57:46 by gpollast          #+#    #+#             */
-/*   Updated: 2025/05/13 16:58:59 by gpollast         ###   ########.fr       */
+/*   Updated: 2025/05/14 11:02:11 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
+
+# include <stdarg.h>
 
 int	ft_printf(const char *p_conv, char *str);
 
