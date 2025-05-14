a14bae6a87634dbbed981417ce22186311e39f0d
Guillaume Pollastri <gpollast@f6r6s5.clusters.42paris.fr>
2025-05-14 11:23:13 +0200

    unistd et stdlib

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 16:57:46 by gpollast          #+#    #+#             */
-/*   Updated: 2025/05/14 11:02:11 by gpollast         ###   ########.fr       */
+/*   Updated: 2025/05/14 11:22:36 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define LIBFTPRINTF_H
 
 # include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 int	ft_printf(const char *p_conv, char *str);
 
