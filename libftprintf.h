a6785ad6cd47aa236a3cdf5db6dbb9afd76d909f
Guillaume Pollastri <gpollast@f6r9s11.clusters.42paris.fr>
2025-05-19 13:16:04 +0200

    1er test francinette

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 16:57:46 by gpollast          #+#    #+#             */
-/*   Updated: 2025/05/19 11:09:20 by gpollast         ###   ########.fr       */
+/*   Updated: 2025/05/19 13:14:41 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,7 @@ int     ft_putadress(void *ptr);
 int     ft_putUnbr_len(unsigned int i);
 int     ft_puthex_low(const unsigned long n);
 int     ft_puthex_up(const unsigned long n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_strchr(const char *s, int c);
 
 #endif
